7e766dcb11e99fa5cacde6f4f5ff8fd635d9e4fc
Arreghini <marreghi@gmail.com>
2024-05-04 15:52:26 -0300

    update Countries

--- cliente/src/components/cardsCountries/CardsCountries.jsx
+++ cliente/src/components/cardsCountries/CardsCountries.jsx
@@ -3,14 +3,13 @@ import { Link, useParams } from 'react-router-dom';
 import earthPattern from '../../assets/imagenTierra1.jpg'; 
 
 const CardsCountry = ({ countriesForPage }) => {
-  const { id } = useParams();
   return (
     <div className="bg-cover bg-center" style={{ backgroundImage: `url(${earthPattern})` }}>
       <div className="flex justify-center text-yellow-100 text-center items-center h-screen">
-           <div className="grid grid-cols-3 gap-6 max-w-screen-lg mt-0">
+        <div className="grid grid-cols-3 gap-6 max-w-screen-lg mt-0">
           {countriesForPage.map((country) => (
-            <Link to={`/detail/${country.id}`} key={country.id}>  
-              <div className="border border-gray-300 rounded-2xl overflow-hidden w-full h-full">
+            <Link to={`/detail/${country.id}`} key={country.id}>
+              <div className="border border-gray-300 rounded-2xl overflow-hidden w-60 h-70">
                 <img className="w-full h-32 object-cover" src={country.flag_image} alt={country.name} />
                 <div className="px-4 py-2">
                   <p className="font-semibold">{country.name}</p>
@@ -24,5 +23,7 @@ const CardsCountry = ({ countriesForPage }) => {
     </div>
   );
 };
+
+
 
 export default CardsCountry;
